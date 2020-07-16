5622ecdf351d2630d93de59e3c75b0631bc40561
czara804 <cbzara@gmail.com>
2020-07-16 13:17:12 +1000

    added axios link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useReducer, useEffect} from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Products from './components/Products'
+import { StateContext} from './config/store'
 
 const App = () => {
+  
+  // initial state for state reducer
+  const initialState = {
+    products: [],
+    // loggedInUser: null
+  }
+
+  // Create state reducer store and dispatcher
+  const [store, dispatch] = useReducer(stateReducer,initialState)
+  const {blogPosts, loggedInUser} = store
+
+  useEffect(() => {
+    dispatch({
+      type: "setProducts",
+      data: products
+    })
+  },[])
+  
+  
   return (
     <div >
-          The basic empty template
+      <StateContext.Provider value={{store, dispatch}} >
+        <BrowserRouter>
+            <h1>The basic empty template</h1>
+            <Route exact path='/products' component={Products} />
+        
+        </BrowserRouter>
+      </StateContext.Provider>
     </div>
   )
 }
---
+++ src/components/Products.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Product from './Product'
+import {useGlobalState} from '../config/store'
+
+const Products = () => {
+    const {store} = useGlobalState()
+    const {products} = store
+    return (
+        <div>
+          {products}
+            {/* {products.map((product) => <Product key={product._id} product={product} />)}         */}
+        </div>
+    )
+}
+
+export default Products
