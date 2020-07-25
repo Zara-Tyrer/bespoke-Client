2293d48628dffa8bcabb7f43cf83db6fd4d107e7
czara804 <56744100+czara804@users.noreply.github.com>
2020-07-25 16:02:31 +1000

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://bespoke-nails.herokuapp.com",
   timeout: 10000,
   withCredentials: true
 })
