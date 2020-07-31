fa9cb46890c01793ebe04f8d04d5ff8b7a1dd5b7
JadeTyrer <jadetyrer@hotmail.co.uk>
2020-07-31 19:04:11 +1000

    changed to deplyed server

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 //creates an axios request to be used in services (connection between client and server)
 
 export default axios.create({
-  // baseURL: "https://bespoke-nails.herokuapp.com/",
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://bespoke-nails.herokuapp.com/",
+  // baseURL: "http://localhost:3001/",
   timeout: 10000,
   withCredentials: true
 })
